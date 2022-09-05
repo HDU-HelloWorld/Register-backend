d9cf99251006a91af3be4c59c41326766aa5750d
HDU_Nbsp <1808347356@qq.com>
2022-09-05 09:22:02 +0800

    后端添加相同手机号的验证码发送时间限制，防止Api攻击

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@ const sequelize = new Sequelize('register-hw', 'helloworld', 'hw2022', {
   dialect: 'postgres',
 })
 class User extends Model { }
+class Verify extends Model { }
 
 
 User.init({
@@ -69,6 +70,31 @@ User.init({
 }, { sequelize, modelName: 'NewMember' })
 User.sync({ alter: true })
 
+// 存放验证码
+Verify.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      is: /^[0-9]{11}$/,
+    }
+  },
+  code: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  time: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  }
+}, { sequelize, modelName: 'Verify' })
+
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   try {
@@ -102,7 +128,15 @@ router.post('/register', async function (req, res, next) {
 
 router.post('/getAuthCode', async function (req, res, next) {
   try {
-    console.log('尝试连接梦网云')
+    // 校验手机号
+    if (!(/^1[3-9][0-9]\d{8}$/.test(phoneNum))) {
+      res.send({
+        msg: '手机号格式不正确',
+        name: 'ValidationError'
+      }, 500)
+      return
+    }
+    // console.log('尝试连接梦网云')
     let Mapi = {
       'apikey': '190f304e388e9516a8d90e38cee777ed',
       'mobile': '',
@@ -113,33 +147,97 @@ router.post('/getAuthCode', async function (req, res, next) {
     phoneNum = data.phone
     timestamp = data.timestamp
     authCode = data.code
-    // 校验手机号
-    if (!(/^1[3-9][0-9]\d{8}$/.test(phoneNum))) {
-      res.send({
-        msg: '手机号格式不正确',
-        name: 'ValidationError'
-      }, 500)
-      return
-    }
-    Mapi.mobile = phoneNum
-    Mapi.content = `验证码：${authCode}，打死都不要告诉别人哦！`
-    // 将Mapi.content内容转为GBK明文格式
-    Mapi.content = encodeGBK(Mapi.content)
-    const options = {
-      url: 'http://api01.monyun.cn:7901/sms/v2/std/single_send',
-      json: true,
-      body: Mapi,
-    }
-    console.log(Mapi)
-    request.post(options, (err, res, body) => {
-      // console.log(req);
-      // console.log(res);
-      console.log(body)
-      console.log(Mapi.content)
-      if (err) {
-        console.log(err)
+    // 查看数据库中是否有该手机号
+    const verify = await Verify.findOne({
+      where: {
+        phone: phoneNum
       }
     })
+    if (verify) {
+      // 如果有，判断验证码是否过期
+      let time = verify.time
+      let now = new Date()
+      let diff = now - time
+      if (diff > 50000) {
+        // 如果过期，则生成新的4位验证码并更新时间
+        let newCode = Math.floor(Math.random() * 9000 + 1000)
+        await Verify.update({
+          code: newCode,
+          time: now
+        }, {
+          where: {
+            phone: phoneNum
+          }
+        })
+        console.log('验证码已更新')
+        // 发送验证码
+        Mapi.mobile = phoneNum
+        Mapi.content = encodeGBK(`验证码：${authCode}，打死都不要告诉别人哦！`)
+        const options = {
+          url: 'http://api01.monyun.cn:7901/sms/v2/std/single_send',
+          json: true,
+          body: Mapi,
+        }
+        request.post(options, (err, res, body) => {
+          // console.log(req);
+          // console.log(res);
+          console.log(body)
+          console.log(Mapi.content)
+          if (err) {
+            console.log(err)
+          }
+        })
+      } else {
+        // 一分钟内不重复发送
+        console.log('一分钟内不重复发送')
+      }
+    } else {
+      // 如果没有，生成新的4位验证码并写入数据库
+      let newCode = Math.floor(Math.random() * 9000 + 1000)
+      await Verify.create({
+        phone: phoneNum,
+        code: newCode,
+        time: new Date()
+      })
+      console.log('验证码已生成')
+      // 发送验证码
+      Mapi.mobile = phoneNum
+      Mapi.content = encodeGBK(`验证码：${authCode}，打死都不要告诉别人哦！`)
+      const options = {
+        url: 'http://api01.monyun.cn:7901/sms/v2/std/single_send',
+        json: true,
+        body: Mapi,
+      }
+      request.post(options, (err, res, body) => {
+        // console.log(req);
+        // console.log(res);
+        console.log(body)
+        console.log(Mapi.content)
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+    // 老版本校验规则
+    // Mapi.mobile = phoneNum
+    // Mapi.content = `验证码：${authCode}，打死都不要告诉别人哦！`
+    // // 将Mapi.content内容转为GBK明文格式
+    // Mapi.content = encodeGBK(Mapi.content)
+    // const options = {
+    //   url: 'http://api01.monyun.cn:7901/sms/v2/std/single_send',
+    //   json: true,
+    //   body: Mapi,
+    // }
+    // console.log(Mapi)
+    // request.post(options, (err, res, body) => {
+    //   // console.log(req);
+    //   // console.log(res);
+    //   console.log(body)
+    //   console.log(Mapi.content)
+    //   if (err) {
+    //     console.log(err)
+    //   }
+    // })
   } catch (error) {
     console.log(error)
   }
